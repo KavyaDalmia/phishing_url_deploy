4de7f2a6ed1ec034b1d0028cd4858d15256ae259
Kavya Dalmia <dalmia.kavya@gmail.com>
2024-05-22 13:23:15 -0700

    added prediction from image functionality

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ import pickle
 from features_extraction import *
 import numpy as np
 from flask import Flask, request, redirect, url_for, render_template, flash
-import pytesseract
+#import pytesseract
 from PIL import Image
 import os
 import requests
 import json
 import re
 
-# API endpoint URL
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+
+# Access the API key
+api_key = os.getenv('API_KEY')
+
+
+
 
 feature_names = ['length', 'check_shortening','check_for_iframe', 'check_for_bar_manipulation','check_for_right_click_disabled', 'web_tracffic']
 def featureExtraction2(url):
@@ -66,6 +76,7 @@ def predict():
         final_prediction = 'Safe url'
     return render_template('result.html', extracted_text=None, prediction=final_prediction)
 
+
 # Define a route to handle image upload form submission
 @app.route('/upload', methods=['POST']) #the /upload willl trigger upload_file func that accepts only POST requests
 def upload_file():
@@ -82,12 +93,29 @@ def upload_file():
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         print(filepath)
         file.save(filepath)
-        text = extract_text_from_image(filepath)
-        return render_template('result.html', extracted_text=text, prediction=None)
+        url = extract_text_from_image(filepath)
+        #print('found a url' + url)
+        # Perform any processing on the input
+        features = featureExtraction2(url)
+        features_array = np.array(features, dtype=np.float32)  # Convert to float32 if needed
+        input_data = np.array([features_array])
+        # Use your model to generate predictions
+        prediction = model.predict(input_data)
+        print(features)
+        rounded_prediction = round(prediction[0][0])
+        print("Rounded Prediction:", rounded_prediction)
+        # Render a template with the prediction
+        if rounded_prediction == 1:
+            final_prediction = 'PHISHING! DO NOT FALL FOR IT, DONT CLICK ON IT'
+        elif rounded_prediction == 0:
+            final_prediction = 'Safe url'
+        return render_template('result.html', extracted_text=url, prediction=final_prediction)
+
+        #return render_template('result.html', extracted_text=text, prediction=None)
 
 def extract_text_from_image(image_path):
     api_url = 'https://api.api-ninjas.com/v1/imagetotext'
-    headers = {'X-Api-Key': 'SUEE2wTjUj+ZgdKGRaUxtA==6Zzmgaxu3kTsB8Pw'}
+    headers = {'X-Api-Key': api_key}
     image_file_descriptor = open(image_path, 'rb')
     files = {'image': image_file_descriptor}
     r = requests.post(api_url, files=files, headers=headers)
@@ -96,16 +124,17 @@ def extract_text_from_image(image_path):
     url_pattern = r"(?:https?://|www\.)\S+|(?<=\s)[\w-]+\.[\w.-]+"
     # Search for URLs in the strings list
     urls_found = [string for string in text_fields if re.search(url_pattern, string)]
-    print(urls_found)
-    extracted_text = urls_found
+    if (urls_found):
+        print('extract func' + urls_found[0])
+    extracted_text = urls_found[0]
     return extracted_text
 
-def look_for_urls(strings_list):
-    # Define a regex pattern for matching URLs
-    url_pattern = r"(?:https?://|www\.)\S+|(?<=\s)[\w-]+\.[\w.-]+"
-    # Search for URLs in the strings list
-    urls_found = [string for string in strings_list if re.search(url_pattern, string)]
-    print(urls_found)
+# def look_for_urls(strings_list):
+#     # Define a regex pattern for matching URLs
+#     url_pattern = r"(?:https?://|www\.)\S+|(?<=\s)[\w-]+\.[\w.-]+"
+#     # Search for URLs in the strings list
+#     urls_found = [string for string in strings_list if re.search(url_pattern, string)]
+#     print(urls_found)
     
 
 if __name__ == '__main__':
